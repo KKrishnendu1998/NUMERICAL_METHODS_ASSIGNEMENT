470ced5a3f4f5a89fce516406c448cf6e2551d40
KKrishnendu1998 <57405192+KKrishnendu1998@users.noreply.github.com>
2019-11-06 23:29:01 +0530

    Delete Fibonacci2.py

--- Fibonacci2.py
+++
@@ -1,6 +0,0 @@
-'''printing first 100 fibonacci numbers by importing the module
-creator : Krishnendu Maji  '''
-import timeit as t
-import fibonacci_module as mod
-mod.fibonacci(100)
-print('the time taken :',t.timeit())
